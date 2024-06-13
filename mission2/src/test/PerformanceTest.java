4d9d88a363d084953e7da373ac340ac869c846cb
newmin0119 <newmin0119@gmail.com>
2024-06-13 18:16:28 +0900

    [FIX] PerformanceTest Case 개수 수정

--- mission2/src/test/PerformanceTest.java
+++ mission2/src/test/PerformanceTest.java
@@ -1,34 +1,25 @@
 package test;
 
-import Books.BookManager;
-import Books.BookManager.*;
-
 public class PerformanceTest {
     private static void test(int minId, int maxId){
         System.out.println("Test Id " + minId + " to " + maxId);
         PerformanceTestTarget performanceTestTarget = new PerformanceTestTarget(minId, maxId);
         System.out.println("\tTest minimum Id");
-        System.out.println("\t\tSearchBook: " + performanceTestTarget.testSearchBook(minId));
-        System.out.println("\t\tSearch_bs: " + performanceTestTarget.testSearch_bs(minId));
+        System.out.println("\t\tSearchBook: " + performanceTestTarget.testSearchBook(minId) + "ms");
+        System.out.println("\t\tSearch_bs: " + performanceTestTarget.testSearch_bs(minId) + "ms");
 
         System.out.println("\tTest maximum Id");
-        System.out.println("\t\tSearchBook: " + performanceTestTarget.testSearchBook(maxId-1));
-        System.out.println("\t\tSearch_bs: " + performanceTestTarget.testSearch_bs(maxId-1));
+        System.out.println("\t\tSearchBook: " + performanceTestTarget.testSearchBook(maxId-1) + "ms");
+        System.out.println("\t\tSearch_bs: " + performanceTestTarget.testSearch_bs(maxId-1) + "ms");
 
         int midId = (maxId + minId) / 2;
         System.out.println("\tTest middle Id");
-        System.out.println("\t\tSearchBook: " + performanceTestTarget.testSearchBook(midId));
-        System.out.println("\t\tSearch_bs: " + performanceTestTarget.testSearch_bs(midId));
+        System.out.println("\t\tSearchBook: " + performanceTestTarget.testSearchBook(midId) + "ms");
+        System.out.println("\t\tSearch_bs: " + performanceTestTarget.testSearch_bs(midId) + "ms");
     }
 
     public static void main(String[] args) {
-        // ID: 0~100
-        test(0,10000);
-
-        // ID: 0~1000
-        test(0,1000000);
-
-        // ID: 0~10000
-        test(0,10000000);
+        // ID: 0~200000
+        test(0,200000);
     }
 }
